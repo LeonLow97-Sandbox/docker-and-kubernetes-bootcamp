15bb13d24fff1cac4b306c3a9cfc031450db6aea
LeonLow97 <lowjiewei@gmail.com>
2023-05-01 22:29:18 +0800

    Using docker volumes with docker compose

--- stephen_grider/frontend/src/App.js
+++ stephen_grider/frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Leon :D
+          Testing Docker Compose Up
         </a>
       </header>
     </div>
